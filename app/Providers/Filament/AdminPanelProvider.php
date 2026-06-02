b9f183cd0753a9e8c7560cc8bd9da9ee1a039ecd
Renan <renanaragao159@gmail.com>
2026-06-01 21:02:00 -0300

    [FTR]

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,12 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+            ->navigationGroups([
+                'Controle de Acesso',
+                'Gestão',
+                'Treino',
+                'Configurações',
+            ])
             ->pages([
                 Dashboard::class,
             ])
