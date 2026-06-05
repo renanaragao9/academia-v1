382cd76b31a7da7e64ad1354309fa10ca5ccfc22
Renan <renanaragao159@gmail.com>
2026-06-05 08:09:39 -0300

    [FTR]

--- app/Filament/Resources/Roles/Pages/EditRole.php
+++ app/Filament/Resources/Roles/Pages/EditRole.php
@@ -3,12 +3,15 @@
 namespace App\Filament\Resources\Roles\Pages;
 
 use App\Filament\Resources\Roles\RoleResource;
+use App\Filament\Resources\Roles\Traits\HandlesRolePermissions;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditRole extends EditRecord
 {
+    use HandlesRolePermissions;
+
     protected static string $resource = RoleResource::class;
 
     protected function getHeaderActions(): array
@@ -17,5 +20,10 @@ class EditRole extends EditRecord
             ViewAction::make(),
             DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $this->savePermissions();
     }
 }
